dc3be371afa3b8aedf4240b540fba0c84eca3d81
juroga <juanjrgast@gmail.com>
2022-10-17 17:42:56 +0200

    Added operations function. It works, except that after pressed the operator button you can only press a 1 digit number

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 //Operation functions
+//Variables
+//Storing variables
+let x = 0;
+let y = 0;
+let operator = "";
+let total = 0;
+
 //Add
 function add (x, y) {
     return x + y;
@@ -21,7 +28,7 @@ function divide (x, y) {
 
 //Operator function 
 function operate (x, operator, y) {
-    if(operator === "+") {
+    if(operator === "add") {
         return add(x, y);
     } else if(operator === "subtract") {
         return subtract(x, y);
@@ -34,16 +41,53 @@ function operate (x, operator, y) {
 
 //Display functions
 const inputScreen = document.querySelector("#input-screen");
+const totalScreen = document.querySelector("#total-screen");
 const numberButton = document.querySelectorAll(".number-button");
+inputScreen.textContent = "";
+totalScreen.textContent = "";
+
+//Switch variable so the calculator knows when it has to reset the inputScreen
+//and store y
+let operation = false;
+
+//Operations on numbers. When operation = false, keep adding numbers
+//on display. Once operation = true, screen back to 0 to start
+//adding numbers from the beginning
 numberButton.forEach((button) => {
 button.addEventListener("click", () => {
-    inputScreen.textContent = button.textContent;
-});
+    if(operation === false) {
+        inputScreen.textContent += button.textContent;
+    } else {
+        inputScreen.textContent = "";
+        inputScreen.textContent += button.textContent;
+    }
+    });
 });
 
 const operatorButton = document.querySelectorAll(".operator-button");
 operatorButton.forEach((button) => {
 button.addEventListener("click", () => {
-    inputScreen.textContent += button.textContent;
+    operator = button.id;
+    x = parseInt(inputScreen.textContent);
+    totalScreen.textContent = inputScreen.textContent + button.textContent;
+    operation = true;
 });
 });
+
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener("click", () => {
+    y = parseInt(inputScreen.textContent);
+    total = operate(x, operator, y);
+    inputScreen.textContent = total;
+    totalScreen.textContent = total;
+    operation = false;
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+    x = 0;
+    y = 0;
+    total = 0;
+    operator = "";
+});
+
